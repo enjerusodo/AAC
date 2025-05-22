48f376ecd778fff1ab7ebfdc5a67eb2cc372cbb0
enjerusodo <72417908+enjerusodo@users.noreply.github.com>
2025-05-22 11:28:06 +0300

    Add files via upload

---
+++ printers.js
@@ -0,0 +1,94 @@
+// Информация взята при помощи поисковика google.com
+let data = [
+    // Лазерные принтеры
+    {
+        "model": "HP LaserJet Pro M404dn",
+        "type": "Laser",
+        "speed": "40 ppm",
+        "resolution": "1200 x 1200 dpi",
+        "duplex": true
+    },
+    {
+        "model": "Canon imageCLASS LBP623Cdw",
+        "type": "Laser",
+        "speed": "28 ppm",
+        "resolution": "2400 x 600 dpi",
+        "duplex": true
+    },
+    {
+        "model": "Brother HL-L2350DW",
+        "type": "Laser",
+        "speed": "32 ppm",
+        "resolution": "2400 x 600 dpi",
+        "duplex": true
+    },
+
+    // Струйные принтеры
+    {
+        "model": "Epson EcoTank ET-3850",
+        "type": "Inkjet",
+        "speed": "15 ppm (black), 8 ppm (color)",
+        "resolution": "5760 x 1440 dpi",
+        "duplex": true
+    },
+    {
+        "model": "HP OfficeJet Pro 9015e",
+        "type": "Inkjet",
+        "speed": "22 ppm (black), 18 ppm (color)",
+        "resolution": "4800 x 1200 dpi",
+        "duplex": true
+    },
+    {
+        "model": "Canon PIXMA G620 MegaTank",
+        "type": "Inkjet",
+        "speed": "13 ppm (black), 6 ppm (color)",
+        "resolution": "4800 x 1200 dpi",
+        "duplex": false
+    },
+
+    // Матричные принтеры
+    {
+        "model": "Epson LQ-310",
+        "type": "Dot Matrix",
+        "speed": "347 cps",
+        "resolution": "360 x 360 dpi",
+        "duplex": false
+    },
+    {
+        "model": "OKI Microline 3311e",
+        "type": "Dot Matrix",
+        "speed": "430 cps",
+        "resolution": "360 x 360 dpi",
+        "duplex": false
+    },
+    {
+        "model": "Lexmark Forms Printer 2591n",
+        "type": "Dot Matrix",
+        "speed": "380 cps",
+        "resolution": "360 x 360 dpi",
+        "duplex": false
+    },
+
+    // Сублимационные принтеры
+    {
+        "model": "Canon SELPHY CP1300",
+        "type": "Dye-Sublimation",
+        "speed": "47 sec per 4x6\" photo",
+        "resolution": "300 x 300 dpi",
+        "duplex": false
+    },
+    {
+        "model": "Kodak Dock Plus",
+        "type": "Dye-Sublimation",
+        "speed": "40 sec per 4x6\" photo",
+        "resolution": "300 x 300 dpi",
+        "duplex": false
+    },
+    {
+        "model": "Fujifilm ASK-500",
+        "type": "Dye-Sublimation",
+        "speed": "53 sec per 4x6\" photo",
+        "resolution": "314 x 314 dpi",
+        "duplex": false
+    }
+];
